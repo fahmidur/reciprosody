f297e6badb3897d48e4db78bae82f66209b1a3cd
syed <syed@HAL.(none)>
2012-10-13 11:34:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reciprosody2::Application.config.secret_token = '620f30c5b0abd757f84d44b079b7286ff466f2f386aa47b7a772d7c460923e9d2e26bc86df81960d1988b03c969c27c5a17ce5aa28c8cae1312103c0e088177c'
