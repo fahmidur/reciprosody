e557f185a032ffc446cf4dd81835a6efdae7b3e4
syed <syed@fahmidur.(none)>
2013-01-12 13:05:35 -0500

    better tabs fix

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,14 +1,29 @@
 $(function() {
 	fixTabs();
 	$(window).bind('resize', fixTabs);
+	tabsCrowded();
 });
 
 function fixTabs() {
-	console.log('fixing tabs');
-
-	if($(window).width() < 1000) {
+	//if($(window).width() < 1000) {
+	if(tabsCrowded()) {
 		$('ul.nav.nav-tabs:first li').addClass('span12');
 	} else {
 		$('ul.nav.nav-tabs:first li').removeClass('span12');
 	}
+}
+function tabsCrowded() {
+	var width = $('ul.nav.nav-tabs:first li.corpus_name').html().length * 14;
+
+	$('ul.nav.nav-tabs:first li a').each(function(el) {
+		var l = ($(this).html() + "").length;
+		console.log($(this).html() + " width = " + width);
+
+		width += l*10 + 25;	
+	});
+	console.log("width = " + width);
+
+	if(width > $(window).width()-100 )
+		return true;
+	return false;
 }
