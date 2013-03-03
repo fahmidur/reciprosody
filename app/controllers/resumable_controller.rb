cd4cde57e54df68ebdc7354f638586cf4aed4d06
syed <syed@fahmidur.(none)>
2013-03-03 00:50:41 -0500

    added @resumableIdentifier to resumableController resumable_upload_ready

--- app/controllers/resumable_controller.rb
+++ app/controllers/resumable_controller.rb
@@ -32,6 +32,7 @@ class ResumableController < ApplicationController
 	# GET /resumable_upload_ready
 	# check to see if an upload is ready
 	def resumable_upload_ready
+		@resumableIdentifier = params[:resumableIdentifier]
 		@resumableFilename = params[:filename]
 		@resumableFileSize = params[:size]
 
