412390605dce78890ac84ac30308c289c1db8243
fahmidur <s.f.reza@gmail.com>
2013-03-27 18:40:24 -0400

    + Made delete box animation time faster  + Fixed Favicon bug by including a simple Favicon into assets

--- app/assets/javascripts/corpus_index.js
+++ app/assets/javascripts/corpus_index.js
@@ -1,7 +1,7 @@
 $(function() {
   $(".corpi_item").hover(function() {
-    $(this).children('.hButton').first().animate({opacity:1.0}, 500);
+    $(this).children('.hButton').first().animate({opacity:1.0}, 200);
   }, function() {
-    $(this).children('.hButton').first().animate({opacity:0.3}, 500);
+    $(this).children('.hButton').first().animate({opacity:0.3}, 200);
   });
 });
