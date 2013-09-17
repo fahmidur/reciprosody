4c669b4f4d6df5631d7e35322ac5e70eb3531758
fahmidur <s.f.reza@gmail.com>
2013-09-17 07:57:49 -0400

    -

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -10,5 +10,9 @@ class GraphicsController < ApplicationController
 		@user_corp_rel = Membership.all
 		@user_pub_rel = PublicationMembership.all
 		@user_tool_rel = ToolMembership.all
+
+		@pub_corp_rel = PublicationCorpusRelationship.all
+		@tool_pub_rel = ToolPublicationRelationship.all
+		@tool_corp_rel = ToolCorpusRelationship.all
 	end
 end
