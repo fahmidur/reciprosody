7976d9292967667329d034963d4e9226b89849a5
fahmidur <s.f.reza@gmail.com>
2014-02-24 19:34:54 -0500

    + click to show on resource#index

--- app/assets/javascripts/resource_index.js
+++ app/assets/javascripts/resource_index.js
@@ -4,4 +4,21 @@ $(function() {
   }, function() {
     $(this).find('.hButton').animate({opacity:0.3}, 200);
   });
+
+  var _over_option_item = false;
+  $('.corpi_item .index-item-option-bar a').mouseenter(function() {
+  	_over_option_item = true;
+  	// console.log(_over_option_item);
+  })
+  .mouseleave(function() {
+  	_over_option_item = false;
+  	// console.log(_over_option_item);
+  });
+
+  $('.corpi_item').click(function(e) {
+  	if(_over_option_item) { return; }
+  	e.preventDefault();
+  	window.location = $(this).data('show');
+  });
+
 });
