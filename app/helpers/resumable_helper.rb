4637431aba2d08928b8ea43c8eafa6c3cba74885
fahmidur <s.f.reza@gmail.com>
2014-06-02 12:30:17 -0400

    + resumable session cleanup uncommented    this was commented out for some testing

--- app/helpers/resumable_helper.rb
+++ app/helpers/resumable_helper.rb
@@ -3,8 +3,7 @@ module ResumableHelper
 		# the upload_token is for old DropLoad uploader
 		session[:upload_token] = SecureRandom.uuid
 
-		# not sure this is the best way to do it
-		#session.delete(:resumable_filenames)
-		#session.delete(:resumable_filepaths)
+		session.delete(:resumable_filenames)
+		session.delete(:resumable_filepaths)
 	end
 end
