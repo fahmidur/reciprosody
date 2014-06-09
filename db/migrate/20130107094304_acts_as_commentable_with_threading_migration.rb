1776460969dd89bc3d05a93a8e8289b59cba77ce
fahmidur <s.f.reza@gmail.com>
2014-06-09 12:41:52 -0400

    + MySQL Error on my production end fixed    this stems from ActsAsCommentable    The error is mySQL specific: TEXT/BLOB cannot have default    this is fixed by removing the default argument in the migration

--- db/migrate/20130107094304_acts_as_commentable_with_threading_migration.rb
+++ db/migrate/20130107094304_acts_as_commentable_with_threading_migration.rb
@@ -4,7 +4,8 @@ class ActsAsCommentableWithThreadingMigration < ActiveRecord::Migration
       t.integer :commentable_id, :default => 0
       t.string :commentable_type, :default => ""
       t.string :title, :default => ""
-      t.text :body, :default => ""
+      #t.text :body, :default => "" # SR: causes MySQL error - text/blob cannot have default in production
+      t.text :body # SR: seems to fix the issue during migrate on production
       t.string :subject, :default => ""
       t.integer :user_id, :default => 0, :null => false
       t.integer :parent_id, :lft, :rgt
